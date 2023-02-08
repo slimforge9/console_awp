674c1de5c3bfdcfdafce22269ad15354ea29fbd9
slimforge9 <slimforge9@gmail.com>
2023-02-08 09:55:02 +0100

    working on profile menu

--- Profile.py
+++ Profile.py
@@ -5,10 +5,11 @@ class Profile:
 
     def __init__(self, name):
         self.name = name
-        self.filename = f'{name.lower()}_data.json'
-        self.profile_data = {}
+        self.profile_data = {'profile_name': self.name}
+        self.filename = f"{self.profile_data['profile_name']}.json"
 
     def collect_data(self):
+        self.profile_data['profile_name'] = input("Wpisz nową nazwę profilu: ")
         self.profile_data['officer_name'] = input("[Policjant] Wpisz swój stopień, imię i nazwisko: ")
         self.profile_data['officer_unit'] = input("[Policjant] jednostka: ")
         self.profile_data['ID'] = input("[Policjant] nr służbowy: ")
@@ -17,10 +18,12 @@ class Profile:
                                                "którego dyspozycji zostaje osoba: ")
 
     def save_profile(self):
+        self.filename = f"{self.profile_data['profile_name']}.json"
         with open(self.filename, "w") as f:
             json.dump(self.profile_data, f)
 
     def load_profile(self):
+        self.filename = f"{self.profile_data['profile_name']}.json"
         try:
             with open(self.filename, "r") as f:
                 self.profile_data = json.load(f)
@@ -28,10 +31,14 @@ class Profile:
             self.save_profile()
 
     def edit_data(self):
+
         self.collect_data()
         self.save_profile()
+        return self.profile_data['profile_name']
 
     def stored_data(self):
         self.load_profile()
         data = self.profile_data
+        print(self.profile_data)
+        print(data)
         return data
--- ProfileMenu.py
+++ ProfileMenu.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 from Profile import Profile
@@ -12,6 +13,7 @@ class ProfileMenu:
                            "np: twój stopień, imię i nazwisko, jednostka, miejscowość sporządzania protokołu."
         self.filename = 'profiles.json'
         self.profiles = []
+        self.prev_profile_name = None
         self.load_profiles()
 
     def save_profile(self):
@@ -23,28 +25,50 @@ class ProfileMenu:
             with open(self.filename, "r") as f:
                 self.profiles = json.load(f)
         except FileNotFoundError:
-            self.profiles = ['empty', 'pusty', 'brak']
+            self.profiles = ['empty', 'pusty', 'brak', '(bez profilu - wpisz ręcznie)']
             self.save_profile()
 
     def chose_profile(self, nr):
         name = self.profiles[nr-1]
         return Profile(name).stored_data()
 
+    def edit_profile(self, nr):
+        self.load_profiles()
+        name = self.profiles[nr - 1]
+        self.prev_profile_name = self.profiles[nr - 1]
+        new_profile_name = Profile(name).edit_data()
+        self.profiles[nr - 1] = new_profile_name
+        self.save_profile()
+
+    def handle_profile(self, profile):
+        choice = input(f"1. Akceptuj profil, 2. Edytuj profil: ")
+        if choice == '1':
+            return profile
+        elif choice == '2':
+            self.edit_profile(profile)
+
     def menu(self):
         while True:
-            choice = input(f"Wybierz profil: 1.{self.profiles[0]}, 2.{self.profiles[1]}, 3. {self.profiles[2]}\n")
+            choice = input(f"Wybierz profil: 1.{self.profiles[0]}, 2.{self.profiles[1]}, 3. {self.profiles[2]},"
+                           f"4. {self.profiles[3]}\n")
             if choice == '1':
                 return 1
             elif choice == '2':
                 return 2
             elif choice == '3':
                 return 3
+            elif choice == '4':
+                return 4
             else:
                 print("Nie wybrales profilu")
 
 
 startMenu = ProfileMenu()
 chosen_profile = startMenu.menu()
+startMenu.edit_profile(chosen_profile)
 # profile_dict = startMenu.chose_profile(chosen_profile)
+# ask = Profile(chosen_profile)
+# ask.edit_data()
+# print(ask.stored_data())
 
 
